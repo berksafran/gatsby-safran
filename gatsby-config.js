4eed4cdf8064c0b3caa80e8a8bad84bc704b23fc
Berk Safranbolulu <berk.safranbolulu@huawei.com>
2021-04-06 19:52:00 +0300

    Initial commit.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Default Starter Tailwind`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@berksafran`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -29,6 +29,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-postcss`, // For using Tailwind CSS with PurgeCSS feature.
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,12 +3,17 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Test from "../components/Test"
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <p className="btn-blue">Welcome to page 2</p>
+    <p className="btn-yellow">Welcome to page 2</p>
+    <hr></hr>
+    <Test />
+    <hr></hr>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
